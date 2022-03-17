bcb390fc1818539ef21c459e585bde193e88d996
daniemoyo <daniemoyo@yahoo.com>
2022-03-17 03:41:03 -0500

    added styling

--- public/app.js
+++ public/app.js
@@ -1,48 +1,52 @@
 
-const currencyIcon = ['','https://www.cryptofonts.com/img/icons/btc.svg',
-'https://www.cryptofonts.com/img/icons/doge.svg',
- 'https://www.cryptofonts.com/img/icons/algo.svg',
-'https://www.cryptofonts.com/img/icons/eth.svg']
-const currency = ['','BITCOIN', 'DOGECOIN', "ALGORAND", "ETHERIUM"]
+/* currency array for icon src */
+const currencyIcon = ['','https://www.cryptofonts.com/img/icons/btc.svg','https://www.cryptofonts.com/img/icons/doge.svg','https://www.cryptofonts.com/img/icons/algo.svg','https://www.cryptofonts.com/img/icons/eth.svg']
+
+const currency = ['','BITCOIN', 'DOGECOIN', "ALGORAND", "ETHERIUM"]/* currency name array */
 const hideMain = ()=>{ $("#main").addClass('d-none') };
 const showMain = ()=>{ $("#main").removeClass('d-none') };
 const hideLogin = ()=>{  $("#login").addClass('d-none') };
 const showLogin = ()=>{  $("#login").removeClass('d-none') };
 
-const $cardGroup = $("#cardGroup");
-
-const $creatAddress = $("#createAddress");
-// checking to see if user is logged in
-let isLoggedIn = false;
+const $cardGroup = $("#cardGroup");/* cards container */
+const $cardAddress = $("#cardAddress");
+const $creatAddress = $("#createAddress"); /* ID for create address button */
+const $btc = $("#1");
+const $doge = $("#2");
+const $algo = $("#3");
+const $eth = $("#4");
+let isLoggedIn = false;/* Check if user is logged in */
 let userObj = {};
 const $loginBtn = $('#loginBtn');
 const $loginOutBtn = $('#logout');
-// //QRCODE --Fetching from server->api endpoint-http://localhost:3000/qrcode
-// // fetch("/qrcode")
-// // .then((res)=> res.json())
-// // .then((data) => {
-// //     document.body.appendChild(img)
-// // })
+
+$cardAddress.on("click", (event)=>{
+    event.stopPropagation();
+    console.log(event.target.innerText);
+    navigator.clipboard.writeText(event.target.innerText)
+  
+})
 // console.log(JSON.parse(sessionStorage.userObj)); IF you want to get sesstion data use JSON.parse(sessionStorage.userObj)
 const dash = ()=>{
-    const $cardAddress = $("#cardAddress");
+    
     const user = JSON.parse(sessionStorage.userObj)
-    $("#imgThumbnail").attr("src", `${currencyIcon[user.wallet[0].currency_id]}`);
+    $("#imgThumbnail").attr("src", `${currencyIcon[1]}`);
     $("#cardTitle").text(`${currency[user.wallet[0].currency_id]} `);
     $("#badgeNum").text(`${user.wallet.length}`);
     // <div id="cardAddressDiv d-flex" class="card-body">
     //     <ul id="cardAddress" class="list-group">
                                     
     //     </ul>
-    // </div>
+    // </div>1HX6tk8TcdFnAb5m5yA99VNGmEL4vDhFfh
     var el=''
     for (let i = 0; i < user.wallet.length; i++) {
-        el += `<li id="walletAddress" class="list-group-item d-flex justify-content-between align-items-center">`+`${user.wallet[i].address} <i id="qr" class="bi bi-qr-code"></i>`+`</li>`
+        el += `<li id="walletAddress" class="list-group-item d-flex justify-content-between align-items-center border-0 engraved">`+`${user.wallet[i].address} <i id="qr" class="bi bi-qr-code"></i>`+`</li>`
     }
     $cardAddress.append(el)
     // return $cardAddress;
 }
 
+/* Generate new Address */
 $creatAddress.on("click",(e)=>{
     const user = JSON.parse(sessionStorage.userObj)
     // e.preventDefault()
@@ -61,7 +65,7 @@ $creatAddress.on("click",(e)=>{
         img.src = data.qrURL
         console.log(img.src);
         const createdPublicAddress  = data.wallet.pri_key;
-        const el = `<li id="walletAddress" class="list-group-item d-flex justify-content-between align-items-center">${data.wallet.address} <i class="bi bi-qr-code"></i></li>`
+        const el = `<li id="walletAddress" class="list-group-item d-flex justify-content-between align-items-center"><a>${data.wallet.address} <i class="bi bi-qr-code"></i></a></li>`
         $cardAddress.append(el);
         $("#badgeNum").text(`${user.wallet.length+1}`)
         Swal.fire({
@@ -75,10 +79,12 @@ $creatAddress.on("click",(e)=>{
         console.log(onRejected);
     })
 })
+//logout of the app
 $loginOutBtn.click(()=>{
     sessionStorage.setItem("user_id", "");
     location.reload();
 })
+//login into the app
 $loginBtn.click((e)=>{
     e.preventDefault()
     let email  = $("#emailLogin").val();
@@ -127,7 +133,7 @@ $loginBtn.click((e)=>{
           })
     })
 })
-
+/* Helper function to enter app */
 const start = (()=>{
     isLoggedIn = sessionStorage.getItem("user_id")
     if (isLoggedIn) {
@@ -142,5 +148,5 @@ const start = (()=>{
     }
     
 })
-
+/* Entry point */
 start();
